b6b4d4c99e7665acf60969874445513d251d5c1e
Jacob Peterson <jsksma2@gmail.com>
2013-09-11 17:52:11 -0700

    TJAppDelegate: handle open url for Instagram, setup login flow

--- 20Jeans-API-Sample/TJAppDelegate.m
+++ 20Jeans-API-Sample/TJAppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "TJAppDelegate.h"
+#import "TJInstagramManager.h"
+#import "TJLoginViewController.h"
 
 @implementation TJAppDelegate
 
@@ -14,10 +16,33 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
+    UIViewController *mainViewController = [[UIViewController alloc] init];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:mainViewController];
+    self.window.rootViewController = navController;
+    
+    if (!TJInstagramManager.shared.isAuthenticated)
+    {
+        TJLoginViewController *loginViewController = [[TJLoginViewController alloc] init];
+        [navController presentViewController:loginViewController animated:NO completion:nil];
+    }
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
     return YES;
+}
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    return [[TJInstagramManager shared] handleOpenURL:url];
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    return [[TJInstagramManager shared] handleOpenURL:url];
 }
 
 @end
