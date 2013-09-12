8c41a24d5b9b52a83851667c07eef3707c02ebe6
Jacob Peterson <jsksma2@gmail.com>
2013-09-11 17:51:31 -0700

    Add TJLoginViewController to project

---
+++ 20Jeans-API-Sample/TJLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  TJInstaLoginViewController.h
+//  20Jeans-API-Sample
+//
+//  Created by Jacob Peterson on 9/11/13.
+//  Copyright (c) 2013 Jacob Peterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJLoginViewController : UIViewController
+
+@end
---
+++ 20Jeans-API-Sample/TJLoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  TJInstaLoginViewController.m
+//  20Jeans-API-Sample
+//
+//  Created by Jacob Peterson on 9/11/13.
+//  Copyright (c) 2013 Jacob Peterson. All rights reserved.
+//
+
+#import "TJLoginViewController.h"
+#import "TJInstagramManager.h"
+
+@interface TJLoginViewController ()
+
+@end
+
+@implementation TJLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
