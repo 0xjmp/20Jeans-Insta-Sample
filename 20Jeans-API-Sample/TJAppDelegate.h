dcb172a4512f20b7c7b94aab30e44df20c86bd7e
Jacob Peterson <jsksma2@gmail.com>
2013-09-11 17:02:27 -0700

    Add Xcode project

---
+++ 20Jeans-API-Sample/TJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TJAppDelegate.h
+//  20Jeans-API-Sample
+//
+//  Created by Jacob Peterson on 9/11/13.
+//  Copyright (c) 2013 Jacob Peterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 20Jeans-API-Sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  20Jeans-API-Sample
+//
+//  Created by Jacob Peterson on 9/11/13.
+//  Copyright (c) 2013 Jacob Peterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TJAppDelegate class]));
+    }
+}
