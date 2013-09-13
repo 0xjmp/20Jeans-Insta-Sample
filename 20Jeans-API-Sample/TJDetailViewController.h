83575b84348732cfd5775a42867d182652ccf4e7
Jacob Peterson <jsksma2@gmail.com>
2013-09-13 12:19:58 -0700

    Add TJDetailViewController to project

---
+++ 20Jeans-API-Sample/TJDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TJDetailViewController.h
+//  20Jeans-API-Sample
+//
+//  Created by Jacob Peterson on 9/13/13.
+//  Copyright (c) 2013 Jacob Peterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJDetailViewController : UIViewController
+
+@end
